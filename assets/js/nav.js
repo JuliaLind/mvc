99c153afc5f4dbc6fb27a1595ca42c94f72b1efc
Julia Lind <juli22@student.bth.se>
2023-04-01 00:26:28 +0200

    additional changes for mobile nav

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,14 +1,13 @@
 export default () => {
-  let myNav = document.getElementById("myTopnav");
-  let hamburger = document.getElementById("nav-toggle");
-  hamburger.addEventListener("click", function () {
-    console.log("clicked");
-    if (myNav.className === "topnav") {
-      myNav.className += " responsive";
-    } else {
-      myNav.className = "topnav";
-    }
-  })
-
-  console.log("tester");
+    let myNav = document.getElementById("myTopnav");
+    let hamburger = document.getElementById("nav-toggle");
+    hamburger.addEventListener("click", function () {
+        console.log("clicked");
+        if (myNav.className === "topnav") {
+        myNav.className += " responsive";
+        } else {
+        myNav.className = "topnav";
+        }
+    })
+    console.log("tester");
 }
