6ddf37916897294d86c864593f9c6d318dc3108b
Julia Lind <juli22@student.bth.se>
2023-04-18 00:20:40 +0200

    updated dealBank method. Before the bank would pick new card based on statistics which is based on the lowest hand value (all aces are valued 1). Now the bank only picks new card as long ac actual hand value is lower than 21

--- src/Game/DealBankTrait.php
+++ src/Game/DealBankTrait.php
@@ -15,11 +15,13 @@ trait DealBankTrait
     public function dealBank(): int
     {
         $bank = $this->bank;
+        $handValue = $bank->handValue();
         $evaluate = -1;
         $risk = 0;
-        while (($risk <= 0.5) && ($this->cardsLeft() > 0)) {
+        while (($risk <= 0.5) && ($this->cardsLeft() > 0) && $handValue < 21) {
             $bank->draw($this->deck);
             $risk = $this->estimateRisk();
+            $handValue = $bank->handValue();
         }
         $evaluate = $this->evaluateBank();
         return $evaluate;
