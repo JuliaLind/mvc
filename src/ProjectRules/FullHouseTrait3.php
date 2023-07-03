b10c0c6e1abfd2819836aa27d3f761615f70a908
Julia Lind <juli22@student.bth.se>
2023-07-03 14:02:33 +0200

    some more refactoring

--- src/ProjectRules/FullHouseTrait3.php
+++ src/ProjectRules/FullHouseTrait3.php
@@ -4,15 +4,15 @@ namespace App\ProjectRules;
 
 trait FullHouseTrait3
 {
-    /**
-     * From FullHouseTrait6
-     *
-     * @param bool $three - false if three of the same rank has not been checked before
-     * @param int $countRank - number of cards of the same rank
-     * @return bool - returns true if three has not been checked previously and
-     * the count of a rank is 3 or 4
-     */
-    abstract private function checkThree(int $three, int $rank): bool;
+    // /**
+    //  * From FullHouseTrait6
+    //  *
+    //  * @param bool $three - false if three of the same rank has not been checked before
+    //  * @param int $countRank - number of cards of the same rank
+    //  * @return bool - returns true if three has not been checked previously and
+    //  * the count of a rank is 3 or 4
+    //  */
+    // abstract private function checkThree(int $three, int $rank): bool;
 
     /**
      * @param array<string> $cards
@@ -30,14 +30,23 @@ trait FullHouseTrait3
          */
         $ranks = $this->countByRank($deck);
 
-        // $three = false;
-        // $two = false;
         $three = 0;
         $two = 0;
-        foreach ($ranks as $rank) {
-            if ($this->checkThree($three, $rank)) {
+        // foreach ($ranks as $rank) {
+        //     if ($this->checkThree($three, $rank)) {
+        //         $three = 1;
+        //     } elseif ($rank >= 2) {
+        //         $two = 1;
+        //     }
+        //     if ($two + $three === 2) {
+        //         return true;
+        //     }
+        // }
+
+        foreach ($ranks as $countRank) {
+            if ($three === 0 && $countRank >= 3) {
                 $three = 1;
-            } elseif ($rank >= 2) {
+            } elseif ($countRank >= 2) {
                 $two = 1;
             }
             if ($two + $three === 2) {
--- src/ProjectRules/FullHouseTrait4.php
+++ src/ProjectRules/FullHouseTrait4.php
@@ -4,28 +4,19 @@ namespace App\ProjectRules;
 
 trait FullHouseTrait4
 {
-    /**
-     * From FullHouseTrait6
-     *
-     * @param bool $three - false if three of the same rank has not been checked before
-     * @param int $countRank - number of cards of the same rank
-     * @return bool - returns true if three has not been checked previously and
-     * the count of a rank is 3 or 4
-     */
-    abstract private function checkThree(int $three, int $countRank): bool;
-
     // /**
-    //  * From FullHouseTrait5
+    //  * From FullHouseTrait6
     //  *
-    //  * @param bool $three - true if three of a kind has been found amongst cards
-    //  * @param bool $two - true if a pair has been found amongst cards (not including
-    //  * the cards in the three)
-    //  * @return bool - returns true if both three and two are true
+    //  * @param bool $three - false if three of the same rank has not been checked before
+    //  * @param int $countRank - number of cards of the same rank
+    //  * @return bool - returns true if three has not been checked previously and
+    //  * the count of a rank is 3 or 4
     //  */
-    // abstract private function checkBoth(bool $three, bool $two): bool;
+    // abstract private function checkThree(int $three, int $countRank): bool;
+
 
     /**
-     * Returns true if it is possible to score a FullHouse¨
+     * Returns true if it is possible to score a FullHouse
      * given the ranks in the hand and all ranks (hand + deck)
      * @param array<int,int> $ranksHand
      * @param array<int,int> $ranksAll
@@ -35,7 +26,9 @@ trait FullHouseTrait4
         $three = 0;
         $two = 0;
         foreach (array_keys($ranksHand) as $rank) {
-            if ($this->checkThree($three, $ranksAll[$rank])) {
+            // if ($this->checkThree($three, $ranksAll[$rank])) {
+
+            if ($three === 0 && $ranksAll[$rank] >= 3) {
                 $three = 1;
             } elseif ($ranksAll[$rank] >= 2) {
                 $two = 1;
