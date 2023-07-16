81cc95ba761a1878e0a39a5cbefdf16061ca719e
Julia Lind <juli22@student.bth.se>
2023-07-16 15:05:51 +0200

    added one more method for the undo last round trait test

--- tests/Project/UndoLastRoundTraitTest.php
+++ tests/Project/UndoLastRoundTraitTest.php
@@ -10,6 +10,9 @@ class UndoLastRoundTraitTest extends TestCase
 {
     use UndoLastRoundTrait;
 
+    /**
+     * Test undo last round when the grids contain more thatn one card each
+     */
     public function testUndoLastRound(): void
     {
         $evaluator = $this->createMock(RuleEvaluator::class);
@@ -41,6 +44,47 @@ class UndoLastRoundTraitTest extends TestCase
         $exp = [
             'house' => [[0, 2]],
             'player' => [[4, 1]],
+        ];
+        $this->assertEquals($exp, $this->lastRound);
+        $this->assertEquals("12D", $this->deck->deal());
+        $this->assertEquals("5S", $this->deck->deal());
+        $this->card = "8H";
+    }
+
+    /**
+     * Test undo last round if both contain one card each and will be empty after
+     */
+    public function testUndoLastRound2(): void
+    {
+        $evaluator = $this->createMock(RuleEvaluator::class);
+        $evaluator->expects($this->exactly(2))->method('results');
+        $this->evaluator = $evaluator;
+        $this->lastRound = [
+            'house' => [[1, 0]],
+            'player' => [[2, 3]],
+        ];
+        $this->card = "5S";
+
+        $house = $this->createMock(Grid::class);
+        $player = $this->createMock(Grid::class);
+        $house->expects($this->once())
+        ->method('removeCard')->with($this->equalTo(1), $this->equalTo(0))->willReturn("12D");
+        $player->expects($this->once())
+        ->method('removeCard')->with($this->equalTo(2), $this->equalTo(3))->willReturn("8H");
+        $this->house = $house;
+        $this->player = $player;
+
+        $factory = $this->createMock(CardFactory::class);
+        $factory->method('fullSet')
+        ->willReturn([]);
+
+        $this->deck = new Deck($factory);
+        $this->undoLastRound();
+
+
+        $exp = [
+            'house' => [],
+            'player' => [],
         ];
         $this->assertEquals($exp, $this->lastRound);
         $this->assertEquals("12D", $this->deck->deal());
--- var/cache/test/App_KernelTestDebugContainer.php
+++ var/cache/test/App_KernelTestDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(App_KernelTestDebugContainer::class, false)) {
 
 return new \ContainerMIPtlAp\App_KernelTestDebugContainer([
     'container.build_hash' => 'MIPtlAp',
-    'container.build_id' => '71e94463',
-    'container.build_time' => 1689511991,
+    'container.build_id' => '68be82e2',
+    'container.build_time' => 1689512725,
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerMIPtlAp');
