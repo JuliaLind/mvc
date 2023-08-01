a5d71bef1c6ab22fd15202a379286a749fdcc39e
Julia Lind <juli22@student.bth.se>
2023-08-01 16:12:30 +0200

    updated reporting text to include the changes in game logic

--- var/cache/test/App_KernelTestDebugContainer.php
+++ var/cache/test/App_KernelTestDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(App_KernelTestDebugContainer::class, false)) {
 
 return new \ContainerMIPtlAp\App_KernelTestDebugContainer([
     'container.build_hash' => 'MIPtlAp',
-    'container.build_id' => '0fbc989e',
-    'container.build_time' => 1690897863,
+    'container.build_id' => '0a2fc7cc',
+    'container.build_time' => 1690898846,
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerMIPtlAp');
